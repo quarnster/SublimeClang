f2906916965e48a56e4724ceaef43036890d3383
Fredrik Ehnbom <quarnster@gmail.com>
2011-12-02 21:48:14 +0100

    Added optional 'recompile as you type' functionality. Weirdness with 'prefix' completions worked around.

--- sublimeclang.py
+++ sublimeclang.py
@@ -34,6 +34,8 @@ class SublimeClangAutoComplete(sublime_plugin.EventListener):
         s.clear_on_change("options")
         s.add_on_change("options", self.load_settings)
         self.load_settings(s)
+        self.recompile_active = False
+        self.auto_complete_active = False
 
     def load_settings(self, s = None):
         global translationUnits
@@ -42,6 +44,8 @@ class SublimeClangAutoComplete(sublime_plugin.EventListener):
             s = sublime.load_settings("clang.sublime-settings")
         self.popupDelay = s.get("popupDelay", 500)
         self.dont_complete_startswith = s.get("dont_complete_startswith", ['operator','~'])
+        self.recompileDelay = s.get("recompileDelay", 1000)
+        self.hide_clang_output = s.get("hide_output_when_empty", False)
 
     def parse_res(self, compRes, prefix):
         #print compRes.kind, compRes.string
@@ -70,15 +74,11 @@ class SublimeClangAutoComplete(sublime_plugin.EventListener):
                     insertion = "%s%s" % (insertion, chunk.spelling)
         return (True, "%s - %s" % (representation, returnType), insertion)
 
-    def on_query_completions(self, view, prefix, locations):
+    def get_translation_unit(self, view):
         global translationUnits 
         global index
-        language = re.search("(?<=source\.)[a-zA-Z0-9+#]+", view.scope_name(locations[0])).group(0)
-        if language != "c++" and language != "c":
-            return []
         if index == None:
             index = cindex.Index.create()
-
         tu = None
         if view.file_name() not in translationUnits:
             s = sublime.load_settings("clang.sublime-settings")
@@ -90,19 +90,30 @@ class SublimeClangAutoComplete(sublime_plugin.EventListener):
             if tu != None:
                 translationUnits[view.file_name()] = tu
         else:
-            tu = translationUnits[view.file_name()]
+            tu = translationUnits[view.file_name()]        
+        return tu
+
+    def on_query_completions(self, view, prefix, locations):
+        global translationUnits 
+        global index
+        language = re.search("(?<=source\.)[a-zA-Z0-9+#]+", view.scope_name(locations[0])).group(0)
+        if language != "c++" and language != "c":
+            return []
+
+        tu = self.get_translation_unit(view)
         if tu == None:
             return []
-        row,col = view.rowcol(locations[0])
+        row,col = view.rowcol(locations[0]-len(prefix)) # Getting strange results form clang if I don't remove prefix
         unsaved_files = []
         if view.is_dirty():
             unsaved_files.append((view.file_name(), str(view.substr(Region(0, 65536)))))
+          
         res = tu.codeComplete(view.file_name(), row+1, col+1, unsaved_files)
         ret = []
         if res != None:
             for diag in res.diagnostics:
                 print diag
-            lastRes = res.results[len(res.results)-1].string
+            #lastRes = res.results[len(res.results)-1].string
             #if "CurrentParameter" in str(lastRes):
             #    for chunk in lastRes:
             #        if chunk.isKindCurrentParameter():
@@ -117,13 +128,39 @@ class SublimeClangAutoComplete(sublime_plugin.EventListener):
         return sorted(ret)
 
     def complete(self):
-        if self.auto_complete:
-            self.auto_complete = False
+        if self.auto_complete_active:
+            self.auto_complete_active = False
             self.view.window().run_command("auto_complete")
 
+    def recompile(self):
+        view = self.view
+        unsaved_files = [(view.file_name(), str(view.substr(Region(0, 65536))))]
+        tu = self.get_translation_unit(view)
+        tu.reparse(unsaved_files)
+        errString = ""
+        show = False
+        if len(tu.diagnostics):
+            errString = ""
+            for diag in tu.diagnostics:
+                f = diag.location
+                err = "%s:%d,%d - %s" % (f.file.name, f.line, f.column, diag.spelling)
+                errString = "%s%s\n" % (errString, err)
+            show = True
+        v = view.window().get_output_panel("clang")
+        e = v.begin_edit()
+        v.insert(e, 0, errString)
+        v.end_edit(e)
+        if show:
+            view.window().run_command("show_panel", {"panel": "output.clang"})
+        elif self.hide_clang_output:
+            view.window().run_command("hide_panel", {"panel": "output.clang"})
+        self.recompile_active = False
 
     def on_modified(self, view):
-        self.auto_complete = False
+        if self.popupDelay <= 0 and self.reparseDelay <= 0:
+            return
+        self.auto_complete_active = False
+        self.recompile
         caret = view.sel()[0].a
         language = re.search("(?<=source\.)[a-zA-Z0-9+#]+", view.scope_name(caret))
         if language == None:
@@ -131,9 +168,14 @@ class SublimeClangAutoComplete(sublime_plugin.EventListener):
         language = language.group(0)
         if language != "c++" and language != "c":
             return
+        if self.recompileDelay > 0 and not self.recompile_active:
+            self.recompile_active = True
+            self.view = view
+            sublime.set_timeout(self.recompile, self.recompileDelay)
+
         caret = view.sel()[0].a
         word = view.substr(Region(view.word(caret).a, caret))
-        if word.endswith(".") or word.endswith("->") or word.endswith("::"):
-            self.auto_complete = True
+        if self.popupDelay > 0 and (word.endswith(".") or word.endswith("->") or word.endswith("::")):
+            self.auto_complete_active = True
             self.view = view
             sublime.set_timeout(self.complete, self.popupDelay) 
