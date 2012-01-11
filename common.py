c59b24a83863365a54e5a2039937202050c2a540
Fredrik Ehnbom <quarnster@gmail.com>
2012-01-11 11:59:14 +0100

    Issue #8. All settings (not just "options") can now be per project

--- common.py
+++ common.py
@@ -3,3 +3,13 @@ import sublime
 
 def get_settings():
     return sublime.load_settings("SublimeClang.sublime-settings")
+
+
+def get_setting(key, default=None):
+    try:
+        s = sublime.active_window().active_view().settings()
+        if s.has("sublimeclang_%s" % key):
+            return s.get("sublimeclang_%s" % key)
+    except:
+        pass
+    return get_settings().get(key, default)
