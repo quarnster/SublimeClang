4306c3b06908829b5619832b6abccd4c60053ea8
Fredrik Ehnbom <quarnster@gmail.com>
2012-01-30 18:57:28 +0100

    Issue #11, static analyzer support WIP.

--- common.py
+++ common.py
@@ -1,5 +1,31 @@
 import sublime
+import threading
+import Queue
 
+class Worker:
+    def __init__(self):
+        self.tasks = Queue.Queue()
+        for i in range(get_cpu_count()):
+            t = threading.Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+    def worker(self):
+        try:
+            # Just so we give time for the editor itself to start
+            # up before we start doing work
+            time.sleep(5)
+        except:
+            pass
+        while True:
+            task, data = self.tasks.get()
+            try:
+                task(data)
+            except:
+                import traceback
+                traceback.print_exc()
+            finally:
+                self.tasks.task_done()
 
 def get_settings():
     return sublime.load_settings("SublimeClang.sublime-settings")
@@ -13,3 +39,12 @@ def get_setting(key, default=None):
     except:
         pass
     return get_settings().get(key, default)
+
+def get_cpu_count():
+    cpus = 1
+    try:
+        import multiprocessing
+        cpus = multiprocessing.cpu_count()
+    except:
+        pass
+    return cpus
