b8d338268551f01f33c43d5e7198e25e5f599dd9
Fredrik Ehnbom <quarnster@gmail.com>
2012-02-02 10:24:17 +0100

    Issue #11. More detailed descriptions and now marks the regions

--- staticanalyzer.py
+++ staticanalyzer.py
@@ -26,6 +26,7 @@ import subprocess
 import sublime
 import sublime_plugin
 import time
+import threading
 import traceback
 import Queue
 from common import get_setting, get_cpu_count, Worker
@@ -105,7 +106,7 @@ class AnalyzerOutputView:
 
     def do_clear(self, data=None):
         self.view = sublime.active_window().get_output_panel("clang_static_analyzer")
-        self.view.settings().set("result_file_regex", "^(.+):([0-9]+):([0-9]+)")
+        self.view.settings().set("result_file_regex", "^[ -]*(.+):([0-9]+):([0-9]+)")
 
     def do_show(self, data=None):
         sublime.active_window().run_command("show_panel", {"panel": "output.clang_static_analyzer"})
@@ -141,11 +142,70 @@ class AnalyzerOutputView:
         except:
             traceback.print_exc()
 
+    def get_view(self):
+        return self.view
+
 
 output_view = AnalyzerOutputView()
 
 
+class Diagnostic:
+    def __init__(self, data, files, line):
+        self.data = data
+        self.files = files
+        self.lines = 0
+        self.line = line
+
+    def format_location(self, loc):
+        return "%s:%d:%d" % (self.files[loc["file"]], loc["line"], loc["col"])
+
+    def format_desc(self, desc):
+        return desc.replace("&apos;", "'")
+
+    def format(self):
+        desc = self.data["description"]
+        desc = self.format_desc(desc)
+
+        output = "%s - %s\n" % (self.format_location(self.data["location"]), desc)
+        for path in self.data["path"]:
+            if path["kind"] == "event":
+                output += "    - %s - %s\n" % (self.format_location(path["location"]), self.format_desc(path["extended_message"]))
+        self.lines = output.count("\n")
+        return output
+
+    def get_ranges(self, row):
+        if row == self.line:
+            ret = []
+            for path in self.data["path"]:
+                if path["kind"] == "event":
+                    for range in path["ranges"]:
+                        ret.append(range)
+            return ret
+        else:
+            i = self.line
+            for path in self.data["path"]:
+                if path["kind"] == "event":
+                    i += 1
+                if i == row:
+                    return path["ranges"]
+        return []
+
+
 class Analyzer(Worker):
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.diags = []
+        self.line = 0
+        super(Analyzer, self).__init__()
+
+    def clear(self):
+        sublime.active_window().active_view().erase_regions("clang.analyzer")
+        output_view.clear()
+        self.lock.acquire()
+        self.line = 0
+        self.diags = []
+        self.lock.release()
+
     def update_settings(self):
         cmdline = get_setting("analyzer_commandline", ["clang", "--analyze", "-o", "-"])
         opts = get_setting("options")
@@ -172,16 +232,19 @@ class Analyzer(Worker):
         cmdline = list(self.cmdline)
         cmdline.append(filename)
 
-        p = subprocess.Popen(cmdline, stdout=subprocess.PIPE)
+        p = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
 
         res = parse(stdout)
         if res != None:
+            self.lock.acquire()
             for diag in res["diagnostics"]:
-                loc = diag["location"]
-                desc = diag["description"]
-                desc = desc.replace("&apos;", "'")
-                output_view.add_line("%s:%d:%d - %s\n" % (res["files"][loc["file"]], loc["line"], loc["col"], desc))
+                d = Diagnostic(diag, res["files"], self.line)
+                self.diags.append(d)
+                output_view.add_line(d.format())
+                self.line += d.lines
+            self.lock.release()
+
             if len(res["diagnostics"]) > 0:
                 output_view.show()
         self.set_status("Analyzing %s done" % filename)
@@ -199,18 +262,67 @@ class Analyzer(Worker):
                 time.sleep(0.25)
         self.tasks.put((self.set_status, "Project analyzed"))
 
+    def get_diagnostic_at_line(self, line):
+        for i in range(len(self.diags)):
+            if self.diags[i].line == line:
+                return self.diags[i]
+            elif self.diags[i].line > line:
+                return self.diags[i - 1]
+        if len(self.diags) > 0:
+            return self.diags[len(self.diags)-1]
+
 
 analyzer = Analyzer()
 
 
 class ClangAnalyzeFile(sublime_plugin.TextCommand):
     def run(self, edit):
-        output_view.clear()
+        analyzer.clear()
         fn = self.view.file_name()
         analyzer.analyze_file(fn)
 
 
 class ClangAnalyzeProject(sublime_plugin.TextCommand):
     def run(self, edit):
-        output_view.clear()
+        analyzer.clear()
         analyzer.analyze_project(self.view.window().folders())
+
+
+class ClangAnalyzeEventListener(sublime_plugin.EventListener):
+    def __init__(self):
+        self.ranges = {}
+
+    def prepare_ranges(self, ranges, lut):
+        self.ranges = {}
+        for range in ranges:
+            f = lut[range[0]["file"]]
+            if f not in self.ranges:
+                self.ranges[f] = []
+            self.ranges[f].append([(range[0]["line"], range[0]["col"]), (range[1]["line"], range[1]["col"])])
+
+    def update_regions(self, f, v):
+        regions = []
+        for range in self.ranges[f]:
+            start = range[0]
+            end = range[1]
+            regions.append(sublime.Region(v.text_point(start[0]-1, start[1]-1), v.text_point(end[0]-1, end[1])))
+        v.show(regions[0])
+        v.add_regions("clang.analyzer", regions, get_setting("marker_analyzer_scope", "invalid"), "", sublime.DRAW_OUTLINED)
+
+    def on_load(self, view):
+        f = view.file_name()
+        if f in self.ranges:
+            self.update_regions(f, view)
+
+    def on_selection_modified(self, view):
+        v = output_view.get_view()
+        if not v is None and view.id() == v.id():
+            region = v.full_line(v.sel()[0].a)
+            v.add_regions("clang.analyze.selection", [region], get_setting("marker_analyzer_output_panel_scope", "invalid"), "", sublime.DRAW_OUTLINED)
+            row, col = v.rowcol(v.sel()[0].a)
+            diag = analyzer.get_diagnostic_at_line(row)
+            self.prepare_ranges(diag.get_ranges(row), diag.files)
+            for f in self.ranges:
+                v = sublime.active_window().open_file(f, sublime.TRANSIENT)
+                if not v.is_loading():
+                    self.update_regions(f, v)
