e1d5b205911b4c0cb8a66ea28f61086d2d579a28
Fredrik Ehnbom <quarnster@gmail.com>
2012-03-23 21:08:26 +0100

    Issue #39 fix. Make sure the settings for the actual view is used.
    For example when previewing a file, it is loaded but not activated
    so the view we want to parse is not the active view.
    
    There's still an issue when starting Sublime Text 2 and a
    previous session is restored. Unclear what, if anything, can
    be done about it though, so the source code comment will have
    to do for now:
    
     Views can apparently be window less, in most instances getting
     the active_window will be the right choice (for example when
     previewing a file), but the one instance this is incorrect
     is during Sublime Text 2 session restore. Apparently it's
     possible for views to be windowless then too and since it's
     possible that multiple windows are to be restored, the
     "wrong" one for this view might be the active one and thus
     ${project_path} will not be expanded correctly.
    
     This will have to remain a known documented issue unless
     someone can think of something that should be done plugin
     side to fix this.

--- common.py
+++ common.py
@@ -2,7 +2,8 @@ import sublime
 import threading
 import time
 import Queue
-import os, re
+import os
+import re
 
 
 class Worker(object):
@@ -42,9 +43,11 @@ def get_settings():
     return sublime.load_settings("SublimeClang.sublime-settings")
 
 
-def get_setting(key, default=None):
+def get_setting(key, default=None, view=None):
     try:
-        s = sublime.active_window().active_view().settings()
+        if view == None:
+            view = sublime.active_window().active_view()
+        s = view.settings()
         if s.has("sublimeclang_%s" % key):
             return s.get("sublimeclang_%s" % key)
     except:
@@ -52,12 +55,26 @@ def get_setting(key, default=None):
     return get_settings().get(key, default)
 
 
-def expand_path(value):
+def expand_path(value, window):
     value = value % ({'home': os.getenv('HOME')})
+    if window == None:
+        # Views can apparently be window less, in most instances getting
+        # the active_window will be the right choice (for example when
+        # previewing a file), but the one instance this is incorrect
+        # is during Sublime Text 2 session restore. Apparently it's
+        # possible for views to be windowless then too and since it's
+        # possible that multiple windows are to be restored, the
+        # "wrong" one for this view might be the active one and thus
+        # ${project_path} will not be expanded correctly.
+        #
+        # This will have to remain a known documented issue unless
+        # someone can think of something that should be done plugin
+        # side to fix this.
+        window = sublime.active_window()
 
     get_existing_files = \
         lambda m: [ path \
-            for f in sublime.active_window().folders() \
+            for f in window.folders() \
             for path in [os.path.join(f, m.group('file'))] \
             if os.path.exists(path) \
         ]
@@ -66,39 +83,41 @@ def expand_path(value):
 
     return value
 
-def complete_path(value):
-    path_init, path_last = os.path.split(value)
-    if path_init[:2] == "-I" and (path_last == "**" or path_last == "*") :
-      starting_path = expand_path(path_init[2:])
-      include_paths = []
-      if os.path.exists(starting_path):
-        if path_last == "*":
-          for dirname in os.listdir(starting_path):
-              if not dirname.startswith("."): # skip directories that begin with .
-                include_paths.append("-I" + os.path.join(starting_path, dirname))
-        elif path_last == "**":
-          for dirpath, dirs, files in os.walk(starting_path):
-            for dirname in list(dirs):
-              if dirname.startswith("."): # skip directories that begin with .
-                dirs.remove(dirname)
-            if dirpath != starting_path:
-              include_paths.append("-I" + dirpath)
-        else:
-          include_paths.append("-I" + starting_path)
-      else:
-        pass # perhaps put some error here?
-      return include_paths
-    else:
-      return [expand_path(value)]
 
-def get_path_setting(key, default=None):
-    value = get_setting(key, default)
+def complete_path(value, window):
+    path_init, path_last = os.path.split(value)
+    if path_init[:2] == "-I" and (path_last == "**" or path_last == "*"):
+        starting_path = expand_path(path_init[2:])
+        include_paths = []
+        if os.path.exists(starting_path):
+            if path_last == "*":
+                for dirname in os.listdir(starting_path):
+                    if not dirname.startswith("."):  # skip directories that begin with .
+                        include_paths.append("-I" + os.path.join(starting_path, dirname))
+            elif path_last == "**":
+                for dirpath, dirs, files in os.walk(starting_path):
+                    for dirname in list(dirs):
+                        if dirname.startswith("."):  # skip directories that begin with .
+                            dirs.remove(dirname)
+                    if dirpath != starting_path:
+                        include_paths.append("-I" + dirpath)
+            else:
+                include_paths.append("-I" + starting_path)
+        else:
+            pass  # perhaps put some error here?
+        return include_paths
+    else:
+        return [expand_path(value, window)]
+
+
+def get_path_setting(key, default=None, view=None):
+    value = get_setting(key, default, view)
     opts = []
     if isinstance(value, str) or isinstance(value, unicode):
-        opts.extend(complete_path(value))
+        opts.extend(complete_path(value, view.window()))
     else:
         for v in value:
-          opts.extend(complete_path(v))
+            opts.extend(complete_path(v, view.window()))
     return opts
 
 
