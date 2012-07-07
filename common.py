99770089db19b1a1632727c8bf692cdaa7af42cb
Fredrik Ehnbom <quarnster@gmail.com>
2012-07-07 08:34:01 +0200

    Re-apply commit 5b8f515f999023d0943de2922bcd5c18d1955ed3 which seems to have been lost.  Issue #44, #89.

--- common.py
+++ common.py
@@ -50,7 +50,7 @@ try:
 
 
     def is_supported_language(view):
-        if view.is_scratch() or not get_setting("enabled", True, view):
+        if view.is_scratch() or not get_setting("enabled", True, view) or view.file_name() == None:
             return False
         language = get_language(view)
         if language == None or (language != "c++" and
